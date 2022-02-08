0c36fd115cafa7b334d8941d40cebb1056b80a15
LaxmiMedegar <99235013+LaxmiMedegar@users.noreply.github.com>
2022-02-08 10:46:13 +0530

    python
    This is our python file

---
+++ pro.py
@@ -0,0 +1,87 @@
+##print("hello")
+##print("xyyzxyzxzxyy".count('yy'))
+
+##print("xyyzxyzxzxyy".count('xyy', 2, 11))
+
+##a=dict()
+##a[1]
+
+##a={'B':5,'A':9,'C':7}
+##sorted(a)
+
+##print('xyyzxxyxyy'.lstrip('xyy'))
+##print('abcdef'.partition('cd'))
+##print('cd'.partition('cd'))
+
+##print('abcdefcdghcd'.split('cd'))
+
+##list1 = [1, 2, 3, 4]
+##list2 = [5, 6, 7, 8]
+## 
+##print(len(list1 + list2))
+
+
+##import json
+##var='{"name":"dhoni","age":33}'
+##print(type(var))
+##
+##var=json.loads(var)
+##print(var)
+##print(type(var))
+##
+##var=json.dumps(var)
+##print(var)
+##print(type(var))
+##var="dhoni"
+##for x in var:
+##    print("dohni")
+
+##var="dhoni"
+##for x in var:
+##     print(x,end="_")
+##   
+##    
+
+##var="dhoni"
+##for x,y in enumerate(var):
+##    print(x)
+##   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
